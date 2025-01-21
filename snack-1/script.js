8852d04f7bad43935c74fd9beeda8d4230362989
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-01-21 14:31:08 +0100

    for each loop

--- snack-1/script.js
+++ snack-1/script.js
@@ -5,5 +5,13 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 
 //genero un ciclo forEach per tirar fuori gli elementi dell'array
+names.forEach((name, index) =>{
 
-//stampo il risultato
+    //stampo il risultato utilizzando il template literal
+    
+    
+    
+    
+});
+
+
