67125c2244c759d1b8849aaf3e7d5720e679e288
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-01-21 15:40:07 +0100

    add filter and complete snack 5

--- snack-5/script.js
+++ snack-5/script.js
@@ -4,5 +4,10 @@ const nums = [2, 8, 4, 7, 12, 87];
 // Risultato: [2, 8, 4, 12]
 
 //utilizzo un ciclo find per generare un'array composta da numeri pari
+const numeriPari = nums.filter((numero) =>{
+    return numero % 2 === 0;
+
+});
 
 //stampo il risultato
+console.log(numeriPari);
