6ca6f190eb3023812d13622c90dce655c03c7e50
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-01-21 15:50:57 +0100

    logic js

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,10 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+//utilizzo ciclo find per creare array nuova con solo studente con id 2
+
+
+
+//stampo il risultato
+
