31d2034062b10157b244de9f554f7d3cba86ada4
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-01-21 14:25:23 +0100

    logic js

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,8 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+
+//genero un ciclo forEach per tirar fuori gli elementi dell'array
+
+//stampo il risultato
