9152931b3344906b8169f159a58a9d66e6b82a7d
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-01-21 15:36:40 +0100

    logic js

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,7 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+//utilizzo un ciclo find per generare un'array composta da numeri pari
+
+//stampo il risultato
