b7d47e59b3d8d5807cd338d5f06358d2bff02ef0
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-01-21 14:58:05 +0100

    logic js

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,10 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+
+//utilizzo il ciclo map per creare una copia dell'array a cui andrò ad aggiungere valori agli elementi
+
+//stampo il risultato
+
+
